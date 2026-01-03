6c44a872bbb793be46d0222702bce2fd9e356a7d
whyrupesh <srupeshkumar800@gmail.com>
2026-01-03 11:40:29 +0530

    swagger added

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,16 +6,52 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Backend.Middleware;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Controllers
 builder.Services.AddControllers();
 
+// Swagger Configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "E-Governance Grievance System API", Version = "v1" });
+
+    // JWT Authentication for Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
 // Auth Service
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<JwtTokenHelper>();
-builder.Services.AddScoped<IGrievanceService,GrievanceService>();
+builder.Services.AddScoped<IGrievanceService, GrievanceService>();
 builder.Services.AddScoped<IOfficerService, OfficerService>();
 builder.Services.AddScoped<ISupervisorService, SupervisorService>();
 builder.Services.AddScoped<IReportService, ReportService>();
@@ -62,12 +98,19 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Enable Middleware for Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowAngular");   
+app.UseCors("AllowAngular");
 
 app.UseMiddleware<ExceptionMiddleware>();
 
